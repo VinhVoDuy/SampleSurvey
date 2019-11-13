26f2ad8db43385f09fbef08621187a21c30dd52e
2359Media <2359media@2359Medias-MacBook-Pro.local>
2019-11-13 17:13:14 +0700

    add average score property to Survey model

--- models/survey.js
+++ models/survey.js
@@ -6,7 +6,13 @@ module.exports = (sequelize, DataTypes) => {
     submissions: DataTypes.INTEGER,
     startTime: DataTypes.DATE,
     endTime: DataTypes.DATE
-  }, {});
+  }, {
+    getterMethods: {
+      avgScore() {
+        return this.submissions === 0 ? 0 : this.totalScore / this.submissions;
+      }
+    }
+  });
   Survey.associate = function (models) {
     // Survey.hasOne(models.Submission);
     // Survey.hasOne(models.Question);
--- routes/survey.js
+++ routes/survey.js
@@ -1,7 +1,15 @@
 const router = require('express').Router();
 const { getSurveyFromEventCode, submmit } = require('../controllers/survey');
+const db = require('../models');
 
-router.get('/:eventCode', getSurveyFromEventCode);
+router.get('/eventCode/:eventCode', getSurveyFromEventCode);
 router.post('/submit', submmit);
+router.get('/avg/:id', async (req, res) => {
+  const survey = await db.Survey.findByPk(req.params.id);
+
+  if (!survey) return res.status(404).send('Survey is not found.');
+
+  return res.json({ avgScore: survey.avgScore });
+})
 
 module.exports = router;
